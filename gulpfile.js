2f3bc9bc890a30960cbe59ed5bd339a0736045dc
Alex Shihkerimov <alexandrshihkerimov@gmail.com>
2020-03-26 12:27:06 +0200

    responsive styles start

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,23 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const watch = require('gulp-watch');
+const autoprefixer = require('gulp-autoprefixer');
+var gcmq = require('gulp-group-css-media-queries');
+
 
 gulp.task('sass-compile', function () {
     return gulp.src('./style/scss/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(gcmq())
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./style/css/'))
 });
+
 
 gulp.task('watch', function () {
     gulp.watch('./style/scss/**/*.scss', gulp.series('sass-compile'))
--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
     // --------------------------- Form validate ----------------------------------- //
 
-    var validator = $('#form').validate({
+    $('#form').validate({
         rules: {
             name: {
                 required: true,
@@ -39,25 +39,63 @@ $(document).ready(function () {
     });
 
 
-
     // --------------------------- Header navigation ----------------------------------- //
 
     function scrollTo() {
 
         var winWidth = $(window).width();
-        var headerHeight = 89;
+        var headerHeight = 99;
 
         if (winWidth < 768) {
-            headerHeight = 59;
+            headerHeight = 79;
         }
 
         $('.scroll-down').click(function () {
             var scroll_el = $(this).attr('href');
+            setActiveLi($(this));
             if ($(scroll_el).length != 0) {
                 $('html, body').animate({ scrollTop: $(scroll_el).offset().top - headerHeight }, 500);
             }
             return false;
         });
     }
+
+    function setActiveLi($el) {
+        if (!$el.hasClass('active')) {
+            $el.parents('.header').find('li.active').removeClass('active');
+            $el.parent('li').addClass('active');
+        }
+    }
+
     scrollTo();
+
+    // --------------------------- Header scroll -------------------------------- //
+
+
+    function headerScroll() {
+        $(window).scroll(function () {
+            let desktopWidth = $(window).width() > 767;
+
+            if ($(document).scrollTop() > 100) {
+                if (desktopWidth) $('.header').addClass('header__scroll');
+            } else {
+                if (desktopWidth) $('.header').removeClass('header__scroll');
+            }
+        });
+    }
+    setTimeout(headerScroll(), 100);
+
+
+    // ------------------------------- Shop hover ---------------------------------------//
+
+    function offerHover() {
+        $offerBtn = $('.offer-btn')
+        $offerBtn.hover(function () {
+            $(this).parents('.offer-item').addClass('hoverClass');
+        });
+        $offerBtn.mouseout(function () {
+            $(this).parents('.offer-item').removeClass('hoverClass');
+        });
+    }
+    offerHover();
 });
